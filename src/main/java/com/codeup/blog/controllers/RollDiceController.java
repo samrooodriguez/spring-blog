845eb864e4395be249411a6a30bd9536915bcfee
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-02-26 12:08:25 -0600

    trying to figure out why my css won't apply to my home page

--- src/main/java/com/codeup/blog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/blog/controllers/RollDiceController.java
@@ -2,6 +2,7 @@ package com.codeup.blog.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,7 +10,7 @@ import java.util.Random;
 
 @Controller
 public class RollDiceController {
-    @GetMapping("/roll-dice")
+    @GetMapping("/roll-dice/home")
     public String userGuess(){
         return "rollDice";
     }
