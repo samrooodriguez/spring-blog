ef6dd028f666d3791583c1c4de446eb599948566
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-03-05 14:33:27 -0600

    restarting the whole thing. It got a little messed up

--- src/main/java/com/codeup/blog/models/AdRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.codeup.blog.models;
-//
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface AdRepository extends CrudRepository<Post, Long> {
-//    Post findByTitle(String title);
-//}
